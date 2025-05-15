7e7b8ee59d0941b69d10146c4b36f5d50d65ef47
kirill <kirotavr@gmail.com>
2025-05-15 15:12:25 +0300

    base trajectory and delta-pos vel

--- test.py
+++ test.py
@@ -10,23 +10,89 @@ from pathlib import Path
 import enum
 from tqdm import tqdm
 
+
 class Resolution(enum.Enum):
   SD = (480, 640)
   HD = (720, 1280)
   UHD = (2160, 3840)
 
-res = Resolution.HD
+res = Resolution.SD
 h, w = res.value
 
 # from robot_descriptions import spot_mj_description
-model_dir = Path("mujoco_menagerie/boston_dynamics_spot/scene.xml")
+model_dir = Path("mujoco_menagerie/boston_dynamics_spot/scene_arm.xml")
 model_xml = model_dir / "scene_arm.xml"
 # model = mujoco.MjModel.from_xml_path(spot_mj_description.MJCF_PATH)
 model = mujoco.MjModel.from_xml_path(str(model_dir))
 data = mujoco.MjData(model)
 
-with mujoco.Renderer(model) as renderer:
-  mujoco.mj_forward(model, data)
-  renderer.update_scene(data)
+model.vis.global_.offheight = h
+model.vis.global_.offwidth = w
+camera = mujoco.MjvCamera()
+mujoco.mjv_defaultFreeCamera(model, camera)
+camera.distance = 3
 
-  media.show_image(renderer.render())
+renderer = mujoco.Renderer(model, height=h, width=w)
+
+res = Resolution.SD
+fps = 30
+duration = 5.0
+ctrl_rate = 2
+ctrl_std = 0.05
+
+# Rendering options for visual and collision geoms.
+vis = mujoco.MjvOption()
+vis.geomgroup[2] = True
+vis.geomgroup[3] = False
+coll = mujoco.MjvOption()
+coll.geomgroup[2] = False
+coll.geomgroup[3] = True
+coll.flags[mujoco.mjtVisFlag.mjVIS_CONVEXHULL] = True
+
+# Sample actuator noise and smooth it.
+nsteps = int(np.ceil(duration / model.opt.timestep))
+perturb = np.random.randn(nsteps, model.nu)
+width = int(nsteps * ctrl_rate / duration)
+kernel = np.exp(-0.5 * np.linspace(-3, 3, width) ** 2)
+kernel /= np.linalg.norm(kernel)
+for i in range(model.nu):
+  perturb[:, i] = np.convolve(perturb[:, i], kernel, mode="same")
+
+np.random.seed(12345)
+
+# Set the desired control point.
+if model.nkey > 0:
+  mujoco.mj_resetDataKeyframe(model, data, 0)
+  ctrl0 = data.ctrl.copy()
+else:
+  mujoco.mj_resetData(model, data)
+  ctrl0 = np.mean(model.actuator_ctrlrange, axis=1)
+
+t = np.array([])
+x, y, z = np.array([]), np.array([]), np.array([])
+vel = np.array([])
+
+frames = []
+for i in tqdm(range(nsteps)):
+  data.ctrl[0:12] = ctrl0[0:12] + ctrl_std * perturb[i, 0:12]
+  t = np.append(t, i)
+  x = np.append(x, data.body("arm_link_sh0").xpos[0])
+  y = np.append(y, data.body("arm_link_sh0").xpos[1])
+  z = np.append(z, data.body("arm_link_sh0").xpos[2])
+  vel = np.append(vel, data.body("arm_link_sh0").cvel)
+  mujoco.mj_step(model, data)
+  if len(frames) < data.time * fps:
+    renderer.update_scene(data, camera)
+    frame = renderer.render().copy().astype(np.uint8)
+    frames.append(frame)
+    # if (len(frames) % 5 == 0):
+    #     print(data.body("arm_link_sh0").xpos)
+    #     print(data.body("arm_link_sh0").cvel, '\n')
+
+media.show_video(frames, fps=fps, loop=False)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x, y, z)
+# plt.plot(t, pos)
+# plt.plot(t, vel)
